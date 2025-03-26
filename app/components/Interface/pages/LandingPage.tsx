257b3e38257b2787d3b175a8ae7cb84497fdac68
Vedika Mujgule <38036622+vedikamujgule@users.noreply.github.com>
2025-03-26 16:50:16 -0700

    [update] routing

--- app/components/Interface/pages/LandingPage.tsx
+++ app/components/Interface/pages/LandingPage.tsx
@@ -64,7 +64,7 @@ const LandingPage = () => {
           <div className="mt-6 flex justify-end">
             <Button
               className="bg-black text-white px-4 py-2 text-sm rounded-full"
-              onClick={() => navigate("/dashboard")}
+              // onClick={() => navigate("/dashboard")}
             >
               Go to Dashboard
             </Button>
